905cf6af766f1da682d6650eaa49d5778b655a60
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-08-21 10:37:22 +0000

    [Add]ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
+    end
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
