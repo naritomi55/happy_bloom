03ebbd4626bc55517b93176a4d9284f109bad1bf
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-08-23 05:23:25 +0000

    [Add]マイページ編集機能実装

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -5,11 +5,12 @@ class User::UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(current_user.id)
   end
 
   def update
-
+    current_user.update(user_params)
+    redirect_to users_my_page_path
   end
 
   def confirm
@@ -26,4 +27,8 @@ class User::UsersController < ApplicationController
     @favorite_posts = PostImage.find(favorites)
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
--- app/javascript/script.js
+++ app/javascript/script.js
@@ -24,3 +24,17 @@ $(document).ready(function () {
     hidePrevious : false
   });
 });
+
+
+window.addEventListener('load', () => {
+    const uploader = document.querySelector('.uploader');
+    uploader.addEventListener('change', (e) => {
+      const file = uploader.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const image = reader.result;
+        document.querySelector('.avatar').setAttribute('src', image);
+      }
+    });
+});
