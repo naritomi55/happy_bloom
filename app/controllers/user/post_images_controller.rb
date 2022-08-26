3cf81dc079eec0c211d676ddbed16f15563df798
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-08-26 02:21:35 +0000

    [Add]投稿一覧画面着手

--- app/controllers/user/post_images_controller.rb
+++ app/controllers/user/post_images_controller.rb
@@ -1,14 +1,28 @@
 class User::PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
   end
 
   def create
-
+    post_image = PostImage.new(post_image_params)
+    post_image.user_id = current_user.id
+    if post_image.save
+      redirect_to post_image_path(post_image.id)
+    else
+      render :new
+    end
   end
 
   def index
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
   end
+
+  private
+  def post_image_params
+    params.require(:post_image).permit(:title, :introduction, :image)
+  end
+
 end
